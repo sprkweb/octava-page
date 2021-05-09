925d459f2545ec39653edff37a951b2b11fd6e70
sprkweb <sprkweb@users.noreply.github.com>
2021-05-09 20:08:28 +0000

    deploy: b880f147c66bfed5372a0f9f4d8e73535cb927a3

--- dist/app.js
+++ dist/app.js
@@ -10,14 +10,29 @@ class App extends React.Component {
     super(props);
     this.state = {
       type: "None",
-      highlightNotes: []
+      highlightNotes: [],
+      rowsTotal: 6
     };
     this.updateOptions = this.updateOptions.bind(this);
+    this.addRow = this.addRow.bind(this);
+    this.removeRow = this.removeRow.bind(this);
   }
   updateOptions(type, highlightNotes) {
     this.setState({type, highlightNotes});
   }
+  addRow() {
+    this.setState((state) => {
+      return {rowsTotal: state.rowsTotal + 1};
+    });
+  }
+  removeRow() {
+    this.setState((state) => {
+      return {rowsTotal: state.rowsTotal - 1};
+    });
+  }
   render() {
+    const rowNums = [...Array(this.state.rowsTotal).keys()];
+    const tuning = ["E", "A", "D", "G", "B"];
     return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(NotesOptions, {
       shapes,
       notes,
@@ -26,11 +41,20 @@ class App extends React.Component {
       className: "container guitar"
     }, /* @__PURE__ */ React.createElement("thead", null, /* @__PURE__ */ React.createElement("tr", null, noteNumbers.map((n) => /* @__PURE__ */ React.createElement("th", {
       key: n
-    }, /* @__PURE__ */ React.createElement("span", null, n))))), /* @__PURE__ */ React.createElement("tbody", null, /* @__PURE__ */ React.createElement(Row, {
-      defaultOpenString: "E",
+    }, /* @__PURE__ */ React.createElement("span", null, n))))), /* @__PURE__ */ React.createElement("tbody", null, rowNums.map((index) => /* @__PURE__ */ React.createElement(Row, {
+      defaultOpenString: tuning[index % tuning.length],
       notes,
-      highlightNotes: this.state.highlightNotes
-    }))), /* @__PURE__ */ React.createElement(AutocompleteList, {
+      highlightNotes: this.state.highlightNotes,
+      key: index
+    })))), /* @__PURE__ */ React.createElement("button", {
+      onClick: this.addRow,
+      type: "button",
+      className: "btn btn-light btn-control-rows"
+    }, "+"), /* @__PURE__ */ React.createElement("button", {
+      onClick: this.removeRow,
+      type: "button",
+      className: "btn btn-light btn-control-rows"
+    }, "−"), /* @__PURE__ */ React.createElement(AutocompleteList, {
       id: "notes",
       list: notes
     }));
