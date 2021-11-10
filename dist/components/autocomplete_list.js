29b90e93b5b622cad4706ccfee28dd026cc0422c
sprkweb <sprkweb@users.noreply.github.com>
2021-11-10 14:47:03 +0000

    deploy: 5d00ccfb4067c21860bb14f33f25408de6445b86

--- dist/components/autocomplete_list.js
+++
@@ -1,9 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-export default function AutocompleteList(p) {
-  return /* @__PURE__ */ React.createElement("datalist", {
-    id: p.id
-  }, p.list.map((note) => /* @__PURE__ */ React.createElement("option", {
-    value: note,
-    key: note
-  })));
-}
