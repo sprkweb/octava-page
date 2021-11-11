5232daf7e179b9f0759098139e2df68083255948
Vadim Saprykin <sprkweb@yandex.ru>
2021-11-11 19:27:51 +0300

    change colors

--- src/components/notes_options.jsx
+++ src/components/notes_options.jsx
@@ -59,7 +59,7 @@ export default class NotesOptions extends React.Component {
                 <div className="shape-type-container">
                     <div className="container shape-form">
                         <RadioButtonGroup 
-                            className="btn-primary btn-outline-light" 
+                            className="btn-outline-light" 
                             name="shape"
                             values={types} 
                             defaultValue="None" 
