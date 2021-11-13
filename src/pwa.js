77ac5ee658775feee4b404f31a54137557c67983
Vadim Saprykin <sprkweb@yandex.ru>
2021-11-13 17:52:45 +0300

    fix path to service worker

--- src/pwa.js
+++ src/pwa.js
@@ -1,7 +1,7 @@
 export function registerPWA() {
     window.addEventListener("load", () => {
         if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("../service-worker.js");
+            navigator.serviceWorker.register("service-worker.js");
         }
     });
 
